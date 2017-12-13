55cbdf3bd54a7e2e24b4f6069a349ce5a0a4744b
Matt Stock <stock@bexkat.com>
2017-12-13 15:28:16 -0500

    corrected all(?) of the relative address index calculations

--- bexkat1/verilator/tests/simple.c
+++ bexkat1/verilator/tests/simple.c
@@ -1,8 +1,12 @@
-unsigned int factorial(unsigned n) {
+unsigned int foo(unsigned n) {
+  return n+6;
 }
 
 int main() {
   int a = 0x45;
   int b = 0x56;
-
+  unsigned int c;
+  
+  c = foo(a);
+  c++;
 }
