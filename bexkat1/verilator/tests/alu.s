0a801dec765d7695749ded2bfd103b162a9d3433
Matt Stock <stock@bexkat.com>
2018-04-15 17:54:20 -0400

    improve and correct tests

--- bexkat1/verilator/tests/alu.s
+++ bexkat1/verilator/tests/alu.s
@@ -11,15 +11,6 @@ _start:
 	subi %8, %5, 0x2
 	lsl %9, %2, %6
 	lsli %10, %0, 0x4
-	ldi %0, -2
-	ldiu %1, 2
-	lsr %11, %0, %1
-	lsri %12, %0, 3
-	asr %11, %0, %1
-	asri %12, %0, 3
-	or %13, %8, %3
-	ori %14, %4, 0x0f0f
-	xor %15, %2, %5
-	xori %14, %2, 0x3113
+	halt
 	halt
 	
--- bexkat1/verilator/tests/pushpop.s
+++ bexkat1/verilator/tests/pushpop.s
@@ -1,14 +1,17 @@
 	.globl _start
 _start:	ldi %sp, 0x00001000
 	ldi %0, 0x12345678
+	ldi %1, 0xffeeddcc
 	push %0
 	nop
 	nop
 	nop
 	nop
 	nop
-	pop %1
-	push %0
 	pop %2
+	push %0
+	push %1
+	pop %3
+	pop %4
 	halt
 	
