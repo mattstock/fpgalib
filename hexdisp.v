fcce54337a65a2d72fb7dd2e1ee4a0222573da3c
Matt Stock <stock@bexkat.com>
2018-04-14 20:45:29 -0400

    new and improved

--- hexdisp.v
+++ hexdisp.v
@@ -1,29 +1,29 @@
-module hexdisp(in, out);
+module hexdisp
+  (input [4:0]            in,
+   output logic [7:0] out);
 
-input [3:0] in;
-output [6:0] out;
+  assign out = { ~in[4], decode };
 
-reg [6:0] out;
-
-always_comb begin
-  case (in)
-    4'h1: out = 'b1111001;
-    4'h2: out = 'b0100100;
-    4'h3: out = 'b0110000;
-    4'h4: out = 'b0011001;
-    4'h5: out = 'b0010010;
-    4'h6: out = 'b0000010;
-    4'h7: out = 'b1111000;
-    4'h8: out = 'b0000000;
-    4'h9: out = 'b0010000;
-    4'ha: out = 'b0001000;
-    4'hb: out = 'b0000011;
-    4'hc: out = 'b1000110;
-    4'hd: out = 'b0100001;
-    4'he: out = 'b0000110;
-    4'hf: out = 'b0001110;
-    4'h0: out = 'b1000000;  
-  endcase
-end
-
+  logic [6:0] 	      decode;
+  
+  always_comb
+    case (in[3:0])
+      4'h1: decode = 'b1111001;
+      4'h2: decode = 'b0100100;
+      4'h3: decode = 'b0110000;
+      4'h4: decode = 'b0011001;
+      4'h5: decode = 'b0010010;
+      4'h6: decode = 'b0000010;
+      4'h7: decode = 'b1111000;
+      4'h8: decode = 'b0000000;
+      4'h9: decode = 'b0010000;
+      4'ha: decode = 'b0001000;
+      4'hb: decode = 'b0000011;
+      4'hc: decode = 'b1000110;
+      4'hd: decode = 'b0100001;
+      4'he: decode = 'b0000110;
+      4'hf: decode = 'b0001110;
+      4'h0: decode = 'b1000000;  
+    endcase // case (in)
+  
 endmodule
---
+++ segctrl.v
@@ -0,0 +1,77 @@
+`include "wb.vh"
+
+module segctrl
+  #(SEG=7)
+  (input            clk_i,
+   input 	    rst_i,
+   if_wb.slave      bus,
+   output [SEG-1:0] out0,
+   output [SEG-1:0] out1,
+   output [SEG-1:0] out2,
+   output [SEG-1:0] out3,
+   output [SEG-1:0] out4,
+   output [SEG-1:0] out5,
+   output [SEG-1:0] out6,
+   output [SEG-1:0] out7);
+
+  assign bus.stall = 1'b0;
+  assign bus.ack = state;
+  assign bus.dat_o = { 27'h0, vals[bus.adr[4:2]][4:0] };
+  assign out0 = hexed[0][SEG-1:0];
+  assign out1 = hexed[1][SEG-1:0];
+  assign out2 = hexed[2][SEG-1:0];
+  assign out3 = hexed[3][SEG-1:0];
+  assign out4 = hexed[4][SEG-1:0];
+  assign out5 = hexed[5][SEG-1:0];
+  assign out6 = hexed[6][SEG-1:0];
+  assign out7 = hexed[7][SEG-1:0];
+  
+  logic [4:0] 	    vals[7:0], vals_next[7:0];
+  logic [7:0] 	    hexed[7:0];
+  
+  logic 	    state, state_next;
+  
+  always_ff @(posedge clk_i or posedge rst_i)
+    if (rst_i)
+      begin
+	state <= 1'b0;
+	for (int i=0; i < 8; i = i + 1)
+	  vals[i] <= 5'h0;
+      end
+    else
+      begin
+	state <= state_next;
+	for (int i=0; i < 8; i = i + 1)
+	  vals[i] <= vals_next[i];
+      end // else: !if(rst_i)
+  
+  always_comb
+    begin
+      state_next = state;
+
+      for (int i=0; i < 8; i = i + 1)
+	vals_next[i] = vals[i];
+
+      case (state)
+	1'h0:
+	  if (bus.cyc && bus.stb)
+	    begin
+	      state_next = 1'b1;
+	      if (bus.we)
+		vals_next[bus.adr[4:2]] = bus.dat_i[4:0];
+	    end
+	1'h1:
+	  state_next = 1'h0;
+      endcase // case (state)
+    end
+
+  hexdisp hexdisp0(.in(vals[0]), .out(hexed[0]));
+  hexdisp hexdisp1(.in(vals[1]), .out(hexed[1]));
+  hexdisp hexdisp2(.in(vals[2]), .out(hexed[2]));
+  hexdisp hexdisp3(.in(vals[3]), .out(hexed[3]));
+  hexdisp hexdisp4(.in(vals[4]), .out(hexed[4]));
+  hexdisp hexdisp5(.in(vals[5]), .out(hexed[5]));
+  hexdisp hexdisp6(.in(vals[6]), .out(hexed[6]));
+  hexdisp hexdisp7(.in(vals[7]), .out(hexed[7]));
+  
+endmodule
