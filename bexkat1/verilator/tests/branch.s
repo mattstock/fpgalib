f27137581c77df8d1f6ca0817b653db57f081de9
Matt Stock <stock@bexkat.com>
2018-04-15 23:15:16 -0400

    more enhanced test

--- bexkat1/verilator/tests/branch.s
+++ bexkat1/verilator/tests/branch.s
@@ -4,15 +4,17 @@ _start:
 	bra brat1
 	ldiu %1, 0x6677
 	halt
+brat2:	mov.l %4, %0
+	bra brat3
 brat1: 	mov.l %1, %0
-	brn done
+	brn err
 	mov.l %2, %0
 	cmp %0, %1
-	bne done
+	bne err
 	ldiu %3, 0x7899
 	beq brat2
-	bra done
-brat2:	ldi %9, 0x11112222
+	bra err
+brat3:	ldi %9, 0x11112222
 	halt
-done:	ldi %0, 0xdeeddead
+err:	ldi %0, 0xdeeddead
 	halt
