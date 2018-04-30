30c74989e087b7dfb3f48a255729cc8f1ec82d69
Matt Stock <stock@bexkat.com>
2018-04-29 21:20:01 -0400

    switch from casex to casez

--- interrupt_encoder.v
+++ interrupt_encoder.v
@@ -8,12 +8,12 @@ module interrupt_encoder(input        clk_i,
 			 output [3:0] cpu_exception);
 
   always_comb
-    casex ({ timer_in, serial0_in })
-      6'b1xxxxx: cpu_exception = EXC_TIMER3;
-      6'b01xxxx: cpu_exception = EXC_TIMER2;
-      6'b001xxx: cpu_exception = EXC_TIMER1;
-      6'b0001xx: cpu_exception = EXC_TIMER0;
-      6'b00001x: cpu_exception = EXC_UART0_RX;
+    casez ({ timer_in, serial0_in })
+      6'b1?????: cpu_exception = EXC_TIMER3;
+      6'b01????: cpu_exception = EXC_TIMER2;
+      6'b001???: cpu_exception = EXC_TIMER1;
+      6'b0001??: cpu_exception = EXC_TIMER0;
+      6'b00001?: cpu_exception = EXC_UART0_RX;
       6'b000001: cpu_exception = EXC_UART0_TX;
       6'b000000: cpu_exception = EXC_RESET;
     endcase // casex ({ timer_in, serial0_in })
