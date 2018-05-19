000324f1a78168dc5e21a83734c3ae4219ebe83c
Matt Stock <stock@bexkat.com>
2018-05-18 21:26:00 -0400

    add support for a bus error/MMU exception

--- interrupt_encoder.v
+++ interrupt_encoder.v
@@ -5,17 +5,19 @@ module interrupt_encoder(input        clk_i,
 			 input [3:0]  timer_in,
 			 input [1:0]  serial0_in,
 			 input 	      enabled,
+			 input 	      mmu,
 			 output [3:0] cpu_exception);
 
   always_comb
-    casez ({ timer_in, serial0_in })
-      6'b1?????: cpu_exception = EXC_TIMER3;
-      6'b01????: cpu_exception = EXC_TIMER2;
-      6'b001???: cpu_exception = EXC_TIMER1;
-      6'b0001??: cpu_exception = EXC_TIMER0;
-      6'b00001?: cpu_exception = EXC_UART0_RX;
-      6'b000001: cpu_exception = EXC_UART0_TX;
-      6'b000000: cpu_exception = EXC_RESET;
+    casez ({ timer_in, serial0_in, mmu })
+      7'b1??????: cpu_exception = EXC_TIMER3;
+      7'b01?????: cpu_exception = EXC_TIMER2;
+      7'b001????: cpu_exception = EXC_TIMER1;
+      7'b0001???: cpu_exception = EXC_TIMER0;
+      7'b00001??: cpu_exception = EXC_UART0_RX;
+      7'b000001?: cpu_exception = EXC_UART0_TX;
+      7'b0000001: cpu_exception = EXC_MMU;
+      7'b0000000: cpu_exception = EXC_RESET;
     endcase // casex ({ timer_in, serial0_in })
 
 endmodule // interrupt_encoder
