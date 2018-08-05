db8a9a9a4d54ad8ac7e1d179e6afda2b5e5cc92b
Matt Stock <stock@bexkat.com>
2018-08-05 14:00:08 -0400

    fix the exception test to support the cache logic

--- bexkat2/verilator/tests/exceptions.s
+++ bexkat2/verilator/tests/exceptions.s
@@ -2,6 +2,7 @@
 _start:
 	ldi %sp, 0x1000
 	# build vector table at 0x100
+	
 	ldi %0, 0xc0000001
 	ldi %1, 0x70001200
 	std.l %0, 0x108
@@ -9,6 +10,12 @@ _start:
 	ldi %1, 0x70001100
 	std.l %0, 0x168
 	std.l %1, 0x16c
+	# Since the instruction bus isn't using the cache,
+	# I need to flush the jump table out.
+	ldd.l %10, 0xf60
+	ldd.l %10, 0x1060
+	ldd.l %10, 0xf00
+	ldd.l %10, 0x1100
 	
 	setint 0x100
 	sti
@@ -19,6 +26,9 @@ _start:
 
 	.org 0x1100
 	ldi %1, 0x12345678
+	# we need to flush out the stack to the memory dump
+	ldd.l %10, 0x1f0
+	ldd.l %10, 0x2f0
 	rti
 	
 	.org 0x1200
