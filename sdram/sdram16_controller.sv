881d6a95d92871046d265cfacb2182ebea5d58c0
Matt Stock <stock@bexkat.com>
2020-01-01 14:14:26 -0500

    not working yet, but it's making some progress

--- sdram/sdram16_controller.sv
+++ sdram/sdram16_controller.sv
@@ -1,19 +1,19 @@
 `include "../wb.vh"
-module sdram16(input 	   	   clk_i,
-	       output 		   mem_clk_o,
-	       input 		   rst_i,
-	       if_wb.slave         bus,
-	       output 		   we_n,
-	       output 		   cs_n,
-	       output 		   cke,
-	       output 		   cas_n,
-	       output 		   ras_n,
-	       output logic [1:0]  dqm,
-	       output logic [1:0]  ba,
-	       output logic 	   databus_dir,
-	       output logic [12:0] addrbus_out,
-	       input [15:0] 	   databus_in,
-	       output logic [15:0] databus_out);
+module sdram16_controller(input 	      clk_i,
+			  output 	      mem_clk_o,
+			  input 	      rst_i,
+			  if_wb.slave         bus,
+			  output 	      we_n,
+			  output 	      cs_n,
+			  output 	      cke,
+			  output 	      cas_n,
+			  output 	      ras_n,
+			  output logic [1:0]  dqm,
+			  output logic [1:0]  ba,
+			  output logic 	      databus_dir,
+			  output logic [12:0] addrbus_out,
+			  input [15:0] 	      databus_in,
+			  output logic [15:0] databus_out);
 
   assign {cs_n, cas_n, ras_n, we_n} = cmd;
   
@@ -35,50 +35,67 @@ module sdram16(input 	   	   clk_i,
 					       S_INIT_MODE,
 					       S_IDLE,
 					       S_ACTIVATE,
+					       S_ACTIVATE_WAIT,
+					       S_REFRESH,
 					       S_READ,
 					       S_READ_WAIT,
 					       S_READ_OUT,
-					       S_REFRESH,
 					       S_READ_OUT2,
 					       S_READ_OUT3,
-					       S_READ_OUT4,
+					       S_READ_OUT4,	
+					       S_READ_OUT5,
+					       S_READ_OUT6,
+					       S_READ_OUT7,
+					       S_READ_OUT8,
+					       S_WRITE,
 					       S_WRITE2,
 					       S_WRITE3,
 					       S_WRITE4,
-					       S_WRITE,
+					       S_WRITE5,
+					       S_WRITE6,
+					       S_WRITE7,
+					       S_WRITE8,
 					       S_WRITE_WAIT,
 					       S_WRITE_WAIT2,
-					       S_WRITE_WAIT3,
-					       S_ACTIVATE_WAIT
+					       S_WRITE_WAIT3
 					       } state_t;
   
   logic 			   select;
-  
-  assign databus_dir = (state == S_WRITE ||
+  logic 			   write_active;
+  logic 			   read_active;
+  logic 			   wordsel;
+
+  assign write_active = (state == S_WRITE ||
 			state == S_WRITE2 ||
 			state == S_WRITE3 ||
-			state == S_WRITE4);
+			state == S_WRITE4 ||
+			state == S_WRITE5 ||
+			state == S_WRITE6 ||
+			state == S_WRITE7 ||
+			state == S_WRITE8); 
+  assign databus_dir = write_active;
   assign mem_clk_o = clk_i;
-  assign bus.ack = (state == S_READ_OUT ||
-		    state == S_READ_OUT2 ||
-		    state == S_READ_OUT3 ||
-		    state == S_READ_OUT4 ||
-                    state == S_WRITE ||
-		    state == S_WRITE2 ||
-		    state == S_WRITE3 ||
-		    state == S_WRITE4);
-  assign bus.stall = 1'b0;
-  assign bus.dat_o = (select & ~bus.we ? databus_in : 32'h0);
+  assign read_active = (state == S_READ_OUT ||
+			state == S_READ_OUT2 ||
+			state == S_READ_OUT3 ||
+			state == S_READ_OUT4 ||
+			state == S_READ_OUT5 ||
+			state == S_READ_OUT6 ||
+			state == S_READ_OUT7 ||
+			state == S_READ_OUT8);
+  assign bus.ack = write_active | read_active;
+  assign bus.dat_o = (select & ~bus.we ? { halfword, databus_in } : 32'h0);
   assign cke = ~rst_i;
   assign select = bus.cyc & bus.stb;
-  assign databus_out = bus.dat_i;
+  assign databus_out = (wordsel ? bus.dat_i[31:16] : bus.dat_i[15:0]);
   
   logic [1:0] 			   ba_next;
   logic [3:0] 			   cmd, cmd_next;
-  state_t                                   state, state_next;
+  state_t                          state, state_next;
   logic [15:0] 			   delay, delay_next;
   logic [12:0] 			   addrbus_out_next;
   logic [1:0] 			   dqm_next;
+  logic [15:0] 			   halfword, halfword_next;
   
   always_ff @(posedge clk_i or posedge rst_i)
     begin
@@ -89,7 +106,8 @@ module sdram16(input 	   	   clk_i,
 	  state <= S_INIT_WAIT;
 	  ba <= 2'b00;
 	  addrbus_out <= 13'h0000;
-	  dqm <= 2'hf;
+	  dqm <= 2'h3;
+	  halfword <= 16'h0000;
 	end
       else
 	begin
@@ -99,6 +117,7 @@ module sdram16(input 	   	   clk_i,
 	  state <= state_next;
 	  addrbus_out <= addrbus_out_next;
 	  dqm <= dqm_next;
+	  halfword <= halfword_next;
 	end
     end
   
@@ -110,6 +129,10 @@ module sdram16(input 	   	   clk_i,
       addrbus_out_next = addrbus_out;
       ba_next = ba;
       dqm_next = dqm;
+      wordsel = 1'b1;
+      bus.stall = 1'b0;
+      halfword_next = halfword;
+      
       case (state)
 	S_INIT_WAIT:
 	  begin
@@ -150,8 +173,8 @@ module sdram16(input 	   	   clk_i,
 	  begin
 	    ba_next = 2'b00;
 	    cmd_next = CMD_MRS;
-	    // CAS = 2, sequential, write/read burst length = 4
-	    addrbus_out_next = 13'b0000000100010;
+	    // CAS = 2, sequential, write/read burst length = 8
+	    addrbus_out_next = 13'b0000000100011;
 	    delay_next = 16'h3;
 	    state_next = S_INIT_MODE_WAIT;
 	  end
@@ -177,12 +200,14 @@ module sdram16(input 	   	   clk_i,
 	      cmd_next = CMD_REFRESH;
 	      addrbus_out_next[10] = 1'b1; // all banks refresh
 	      delay_next = 16'h6;
+	      bus.stall = 1'h1;
 	      state_next = S_REFRESH;
 	    end
 	S_REFRESH: 
 	  begin
 	    cmd_next = CMD_NOP;
 	    delay_next = delay - 1'b1;
+	    bus.stall = 1'h1;
 	    if (delay == 16'h0)
               state_next = S_IDLE;
 	  end
@@ -208,24 +233,81 @@ module sdram16(input 	   	   clk_i,
 	S_READ_WAIT:
 	  state_next = S_READ_OUT;
 	S_READ_OUT:
-	  state_next = S_READ_OUT2;
+	  begin
+	    bus.stall = 1'b1;
+	    halfword_next = databus_in[15:0];
+	    state_next = S_READ_OUT2;
+	  end
 	S_READ_OUT2:
 	  state_next = S_READ_OUT3;
 	S_READ_OUT3:
-	  state_next = S_READ_OUT4;
+	  begin
+	    bus.stall = 1'b1;
+	    halfword_next = databus_in[15:0];
+	    state_next = S_READ_OUT4;
+	  end
 	S_READ_OUT4:
+	  state_next = S_READ_OUT5;
+	S_READ_OUT5:
+	  begin
+	    bus.stall = 1'b1;
+	    halfword_next = databus_in[15:0];
+	    state_next = S_READ_OUT6;
+	  end
+	S_READ_OUT6:
+	  state_next = S_READ_OUT7;
+	S_READ_OUT7:
+	  begin
+	    bus.stall = 1'b1;
+	    halfword_next = databus_in[15:0];
+	    state_next = S_READ_OUT8;
+	  end
+	S_READ_OUT8:
 	  state_next = S_IDLE;
 	S_WRITE:
 	  begin
+	    bus.stall = 1'b1;
 	    cmd_next = CMD_NOP;
 	    state_next = S_WRITE2;
 	  end
 	S_WRITE2:
-	  state_next = S_WRITE3;
-	S_WRITE3: 
-	  state_next = S_WRITE4;
+	  begin
+	    wordsel = 1'b0;
+	    state_next = S_WRITE3;
+	  end
+	S_WRITE3:
+	  begin
+	    bus.stall = 1'b1;
+	    cmd_next = CMD_NOP;
+	    state_next = S_WRITE4;
+	  end
 	S_WRITE4:
-	  state_next = S_WRITE_WAIT;
+	  begin
+	    wordsel = 1'b0;
+	    state_next = S_WRITE5;
+	  end
+	S_WRITE5:
+	  begin
+	    bus.stall = 1'b1;
+	    cmd_next = CMD_NOP;
+	    state_next = S_WRITE6;
+	  end
+	S_WRITE6:
+	  begin
+	    wordsel = 1'b0;
+	    state_next = S_WRITE7;
+	  end
+	S_WRITE7:
+	  begin
+	    bus.stall = 1'b1;
+	    cmd_next = CMD_NOP;
+	    state_next = S_WRITE8;
+	  end
+	S_WRITE8:
+	  begin
+	    wordsel = 1'b0;
+	    state_next = S_WRITE_WAIT;
+	  end
 	S_WRITE_WAIT: 
 	  state_next = S_WRITE_WAIT2;
 	S_WRITE_WAIT2:
--- sdram/sdram16_controller_cache.sv
+++ sdram/sdram16_controller_cache.sv
@@ -1,43 +1,72 @@
 `include "../wb.vh"
 
-module sdram16_controller_cache(input 	      clk_i,
-				output 	      mem_clk_o,
-				input 	      rst_i,
-				if_wb.slave   bus,
-				output 	      we_n,
-				output 	      cs_n,
-				output 	      cke,
-				output 	      cas_n,
-				output 	      ras_n,
-				output [1:0]  dqm,
-				output [1:0]  ba,
-				output [12:0] addrbus_out,
-				output 	      databus_dir,
-				input [15:0]  databus_in,
-				output [15:0] databus_out);
+`define CACHE_DISABLE
 
-  if_wb sdram();
-   
-  cache cache0(.clk_i(clk_i),
+module sdram16_controller_cache
+  #(AWIDTH=26,
+    DWIDTH=32)
+  (input 	       clk_i,
+   output 	       mem_clk_o,
+   input 	       rst_i,
+		       if_wb.slave bus0,
+		       if_wb.slave bus1,
+		       if_wb.slave stats_bus,
+   output 	       we_n,
+   output 	       cs_n,
+   output 	       cke,
+   output 	       cas_n,
+   output 	       ras_n,
+   output [1:0]        dqm,
+   output [1:0]        ba,
+   output [12:0]       addrbus_out,
+   output 	       databus_dir,
+   input [DWIDTH-1:0]  databus_in,
+   output [DWIDTH-1:0] databus_out,
+   output [1:0]        cache_status);
+
+  if_wb arb_bus();
+`ifndef CACHE_DISABLE
+  if_wb sdram0_bus();
+`endif
+ 
+  arbiter arb0(.clk_i(clk_i),
 	       .rst_i(rst_i),
-	       .inbus(bus.slave),
-	       .outbus(sdram.master),
-	       .stats_stb_i(1'b0));
-   
-  sdram16 sdram0(.clk_i(clk_i),
-		 .rst_i(rst_i),
-		 .bus(sdram.slave),
-		 .mem_clk_o(mem_clk_o), 
-		 .we_n(we_n),
-		 .cs_n(cs_n),
-		 .cke(cke),
-		 .cas_n(cas_n),
-		 .ras_n(ras_n),
-		 .dqm(dqm),
-		 .ba(ba),
-		 .addrbus_out(addrbus_out),
-		 .databus_dir(databus_dir),
-		 .databus_in(databus_in),
-		 .databus_out(databus_out));
+	       .in0(bus0.slave),
+	       .in1(bus1.slave),
+	       .out(arb_bus.master));
+
+`ifndef CACHE_DISABLE   
+  cache
+    #(.AWIDTH(AWIDTH),
+      .DWIDTH(DWIDTH))
+  cache0(.clk_i(clk_i),
+	 .rst_i(rst_i),
+	 .inbus(arb_bus.slave),
+	 .outbus(sdram0_bus.master),
+	 .stats(stats_bus.slave),
+	 .cache_status(cache_status));
+`else 
+  assign cache_status = 2'h0;
+`endif
+  
+  sdram16_controller sdram0(.clk_i(clk_i),
+			    .rst_i(rst_i),
+`ifdef CACHE_DISABLE
+			    .bus(arb_bus.slave),
+`else
+			    .bus(sdram0_bus.slave),
+`endif
+			    .mem_clk_o(mem_clk_o), 
+			    .we_n(we_n),
+			    .cs_n(cs_n),
+			    .cke(cke),
+			    .cas_n(cas_n),
+			    .ras_n(ras_n),
+			    .dqm(dqm),
+			    .ba(ba),
+			    .addrbus_out(addrbus_out),
+			    .databus_dir(databus_dir),
+			    .databus_in(databus_in),
+			    .databus_out(databus_out));
    
 endmodule
