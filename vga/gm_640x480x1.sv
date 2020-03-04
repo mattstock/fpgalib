eda72b3c454805afa0cd05e2299c70ad83a52761
Matt Stock <stock@bexkat.com>
2020-03-03 20:58:05 -0500

    working monochrome, color and grayscale 640x480 modes.  Note that current RAM limitations means that there isn't enough syncram on the FPGA to do the full 640x480x8 VRAM.  This is a placeholder mode, and I'll be adding the 320x200 modes shortly which will fit.

--- vga/gm_640x480x1.sv
+++ vga/gm_640x480x1.sv
@@ -1,6 +1,6 @@
 `include "../wb.vh"
 
-module gm_new
+module gm_640x480x1
   #(BPP = 8)
   (
    input 	    clk_i,
@@ -13,7 +13,7 @@ module gm_new
    output [BPP-1:0] red,
    output [BPP-1:0] green,
    output [BPP-1:0] blue,
-		    if_wb.master bus);
+   if_wb.master     bus);
 
   logic [31:0] 	bus_dat_i, bus_dat_o;
 `ifdef NO_MODPORT_EXPRESSIONS
@@ -29,20 +29,28 @@ module gm_new
    * domains in both directions.  To keep the code as clean as possible, we'll
    * start with the CPU side where we load the local buffer, and then use a
    * different block below that for the VGA and rendering side.
+   * 
+   * For this graphics mode, we use one bit per pixel, meaning that we
+   * need to load in 20 32-bit words per line.  We trigger the load from
+   * memory when we're in the active vertical window and we see the end of
+   * the horizontal window.  We want to start loading data as quickly as we
+   * can, since in the current work, the CPU clock is running at less than
+   * half the speed of the VGA clock.
    */
   
-  typedef enum 	bit [2:0] { S_IDLE, S_BUS, S_STORE, S_ACK_WAIT } state_t;
+  typedef enum 	bit [1:0] { S_IDLE, S_BUS, S_ACK_WAIT } state_t;
   
   state_t      state, state_next;
-  logic [6:0]  idx, idx_next;
+  logic [4:0]  idx, idx_next;
   logic [31:0] rowval, rowval_next;
-  logic [1:0]  cpu_eol_sync;
-  logic        cpu_eol;
-  
-  logic [31:0] fifo_in, fifo_in_next, fifo_out;
+  logic [2:0]  cpu_ha_sync, cpu_va_sync;
+  logic        cpu_ha_falling, cpu_va_rising;
+  logic [31:0] fifo_out;
   logic        fifo_read;
-
-  assign cpu_eol = cpu_eol_sync[0];
+  logic [4:0]  ack_count, ack_count_next;
+  
+  assign cpu_ha_falling = cpu_ha_sync[1:0] == 2'b01;
+  assign cpu_va_rising = cpu_va_sync[1:0] == 2'b10;
   assign bus.cyc = (state == S_BUS || state == S_ACK_WAIT);
   assign bus.stb = (state == S_BUS);
   assign bus.adr = rowval + { idx, 2'h0 };
@@ -55,18 +63,20 @@ module gm_new
       if (rst_i)
 	begin
 	  state <= S_IDLE;
-	  idx <= 7'h0;
+	  idx <= 5'h0;
 	  rowval <= 32'h0;
-	  cpu_eol_sync <= 2'h0;
-	  fifo_in <= 32'h0;
+	  cpu_ha_sync <= 3'h0;
+	  cpu_va_sync <= 3'h0;
+	  ack_count <= 5'h0;
 	end
       else
 	begin
 	  idx <= idx_next;
 	  state <= state_next;
 	  rowval <= rowval_next;
-	  cpu_eol_sync <= { eol, cpu_eol_sync[1] };
-	  fifo_in <= fifo_in_next;
+	  cpu_ha_sync <= { h_active, cpu_ha_sync[2], cpu_ha_sync[1] };
+	  cpu_va_sync <= { v_active, cpu_va_sync[2], cpu_va_sync[1] };
+	  ack_count <= ack_count_next;
 	end
     end
 
@@ -75,103 +85,89 @@ module gm_new
       state_next = state;
       idx_next = idx;
       rowval_next = rowval;
-      fifo_in_next = fifo_in;
+      ack_count_next = ack_count;
+      
+      if (cpu_va_rising)
+	begin
+	  rowval_next = 16'h0;
+	end
       
       case (state)
 	S_IDLE:
 	  begin
-	    if (cpu_eol)
+	    if (cpu_ha_falling && cpu_va_sync[1:0] == 2'b11)
 	      begin
 		state_next = S_BUS;
+		ack_count_next = 5'h0;
 	      end
 	  end
 	S_BUS:
-	  state_next = S_ACK_WAIT;
-	S_ACK_WAIT:
 	  begin
+	    idx_next = idx + 5'd1;
+	    if (idx == 5'd19) // 32 pixels per word, 20 words per line
+	      begin
+		state_next = S_ACK_WAIT;
+	      end
 	    if (bus.ack)
 	      begin
-		state_next = S_STORE;
-		fifo_in_next = bus_dat_i;
+		ack_count_next = ack_count + 5'h1;
 	      end
 	  end
-	S_STORE:
+	S_ACK_WAIT:
 	  begin
-	    if (idx != 7'd80) // 4 pixels per word, 80 words per line
+	    if (ack_count == 5'd19)
 	      begin
-		state_next = S_BUS;
-		idx_next = idx + 7'd1;
-	      end
-	    else
-	      begin
-		idx_next = 7'd0;
-		if (rowval == 'd480*'d80)
-		  begin
-		    rowval_next = 16'h0;
-		  end
-		else
-		  begin
-		    rowval_next = rowval + 10'd80;
-		  end
+		idx_next = 5'd0;
+		rowval_next = rowval + 7'h50;
 		state_next = S_IDLE;
+	      end
+	    if (bus.ack)
+	      begin
+		ack_count_next = ack_count + 5'h1;
 	      end
 	  end
       endcase
     end
 
   /* Now the VGA clocked logic */
-  logic [9:0]  x, x_next;
+  logic [4:0]  x, x_next;
   logic        v_active, h_active;
-  logic        eos, eol;
   
   assign blank_n = v_active & h_active;
-  
-  always_ff @(posedge video_clk_i or posedge eos)
+
+  always_ff @(posedge video_clk_i)
     begin
-      if (eos)
-	begin
-	  x <= 10'h0;
-	end
-      else
-	begin
-	  x <= x_next;
-	end
+      x <= (h_active ? x_next : 5'h0);
     end
   
   always_comb
     begin
-      x_next = (h_active ? x + 10'h1 : 10'h0);
+      x_next = (h_active ? x + 5'h1 : 5'h0);
       fifo_read = 1'h0;
 
-      case (x[2:1])
-	2'h0: { red, green, blue } = {3{fifo_out[7:0]}};
-	2'h1: { red, green, blue } = {3{fifo_out[15:8]}};
-	2'h2: { red, green, blue } = {3{fifo_out[23:16]}};
-	2'h3: 
-	  begin
-	    { red, green, blue } = {3{fifo_out[31:24]}};
-	    fifo_read = 1'h1;
-	  end
-      endcase // case (x[2:1])
+      { red, green, blue } = (fifo_out[5'h1f-x] ? 24'hffffff : 24'h0);
+
+      if (h_active && v_active && x == 5'h1f)
+	begin
+	  fifo_read = 1'h1;
+	end
     end
 
   dualfifo
-    #(.AWIDTH(7))
+    #(.AWIDTH(5))
     fifo0(.wclk_i(clk_i),
 	  .wrst_i(rst_i),
 	  .rclk_i(video_clk_i),
 	  .rrst_i(video_rst_i),
-	  .write(state == S_STORE),
+	  .write(bus.cyc && bus.ack),
 	  .read(fifo_read),
-	  .in(fifo_in),
+	  .in(bus_dat_i),
 	  .out(fifo_out));
   
   vga_controller25 vga13h(.v_active(v_active),
 			  .h_active(h_active),
 			  .vs(vs),
 			  .hs(hs),
-			  .eos(eos),
-			  .eol(eol),
 			  .clock(video_clk_i),
 			  .rst_i(video_rst_i));
 
--- vga/vga_master.sv
+++ vga/vga_master.sv
@@ -31,8 +31,9 @@ module vga_master
   logic [31:0] 	    outbus_dat_o, outbus_dat_i;
   
   if_wb textbus();
-  if_wb gm_monobus();
-  if_wb gm_13hbus();
+  if_wb gm_640x480x1bus();
+  if_wb gm_640x480x8bus();
+  if_wb palettebus();
   
 `ifdef NO_MODPORT_EXPRESSIONS
   assign inbus_dat_i = inbus.dat_m;
@@ -60,13 +61,13 @@ module vga_master
   logic 	    blank28_n;
   
   logic [15:0] 	    x25_raw, y25_raw;
-  logic [BPP-1:0]   gm_mono_r, gm_mono_g, gm_mono_b;
+  logic [BPP-1:0]   gm_640x480x1_r, gm_640x480x1_g, gm_640x480x1_b;
   logic 	    vs25, hs25;
   logic 	    blank25_n;
-  logic [BPP-1:0]   gm_13h_r, gm_13h_g, gm_13h_b;
+  logic [BPP-1:0]   gm_640x480x8_r, gm_640x480x8_g, gm_640x480x8_b;
   logic 	    vs_13h, hs_13h;
   logic 	    blank_13h_n;
-  logic [1:0] 	    gm_active;
+  logic [2:0] 	    gm_active;
   
   assign sync_n = 1'b0;
   
@@ -75,52 +76,74 @@ module vga_master
   assign textbus.ack = outbus.ack;
   assign textbus.stall = outbus.stall;
   assign textbus.dat_s = outbus_dat_i;
-  assign gm_monobus.dat_s = outbus_dat_i;
-  assign gm_monobus.ack = outbus.ack;
-  assign gm_monobus.stall = outbus.stall;
-  assign gm_13hbus.dat_s = outbus_dat_i;
-  assign gm_13hbus.ack = outbus.ack;
-  assign gm_13hbus.stall = outbus.stall;
+  assign gm_640x480x1bus.dat_s = outbus_dat_i;
+  assign gm_640x480x1bus.ack = outbus.ack;
+  assign gm_640x480x1bus.stall = outbus.stall;
+  assign gm_640x480x8bus.dat_s = outbus_dat_i;
+  assign gm_640x480x8bus.ack = outbus.ack;
+  assign gm_640x480x8bus.stall = outbus.stall;
 
-  assign gm_active = setupreg[1:0];
+  assign palettebus.adr = inbus.adr;
+  assign palettebus.cyc = inbus.cyc;
+  assign palettebus.sel = inbus.sel;
+  assign palettebus.we = inbus.we;
+  assign palettebus.dat_m = inbus_dat_i;
+
+  assign gm_active = setupreg[2:0];
   
   // timing changes based on graphics mode
   // we likely will need to gate this so that we don't interrupt
   // a bus cycle
   always_comb
     begin
-      case (setupreg[1:0])
-	2'h0: // 640x320x1 60Hz graphics
+      case (gm_active)
+	3'h0: // 640x480x1 60Hz graphics
 	  begin
-	    r = gm_mono_r;
-	    g = gm_mono_g;
-	    b = gm_mono_b;
+	    r = gm_640x480x1_r;
+	    g = gm_640x480x1_g;
+	    b = gm_640x480x1_b;
 	    vga_clock = vga_clock25;
 	    vs = vs25;
 	    hs = hs25;
 	    blank_n = blank25_n;
-	    outbus.cyc = gm_monobus.cyc;
-	    outbus.adr = gm_monobus.adr;
-	    outbus.stb = gm_monobus.stb;
-	    outbus.we = gm_monobus.we;
-	    outbus.sel = gm_monobus.sel;
-	    outbus_dat_o = gm_monobus.dat_m;
+	    outbus.cyc = gm_640x480x1bus.cyc;
+	    outbus.adr = gm_640x480x1bus.adr;
+	    outbus.stb = gm_640x480x1bus.stb;
+	    outbus.we = gm_640x480x1bus.we;
+	    outbus.sel = gm_640x480x1bus.sel;
+	    outbus_dat_o = gm_640x480x1bus.dat_m;
 	  end
-	2'h1: // 640x400x8 70Hz graphics, with doubling
+	3'h1: // 640x480x8 60Hz palette
 	  begin
-	    r = gm_13h_r;
-	    g = gm_13h_g;
-	    b = gm_13h_b;
+	    r = gm_640x480x8_r;
+	    g = gm_640x480x8_g;
+	    b = gm_640x480x8_b;
 	    vga_clock = vga_clock25;
 	    vs = vs_13h;
 	    hs = hs_13h;
 	    blank_n = blank_13h_n;
-	    outbus.cyc = gm_13hbus.cyc;
-	    outbus.adr = gm_13hbus.adr;
-	    outbus.stb = gm_13hbus.stb;
-	    outbus.we = gm_13hbus.we;
-	    outbus.sel = gm_13hbus.sel;
-	    outbus_dat_o = gm_13hbus.dat_m;
+	    outbus.cyc = gm_640x480x8bus.cyc;
+	    outbus.adr = gm_640x480x8bus.adr;
+	    outbus.stb = gm_640x480x8bus.stb;
+	    outbus.we = gm_640x480x8bus.we;
+	    outbus.sel = gm_640x480x8bus.sel;
+	    outbus_dat_o = gm_640x480x8bus.dat_m;
+	  end
+	3'h2: // 640x480x8 60Hz grayscale
+	  begin
+	    r = gm_640x480x8_r;
+	    g = gm_640x480x8_g;
+	    b = gm_640x480x8_b;
+	    vga_clock = vga_clock25;
+	    vs = vs_13h;
+	    hs = hs_13h;
+	    blank_n = blank_13h_n;
+	    outbus.cyc = gm_640x480x8bus.cyc;
+	    outbus.adr = gm_640x480x8bus.adr;
+	    outbus.stb = gm_640x480x8bus.stb;
+	    outbus.we = gm_640x480x8bus.we;
+	    outbus.sel = gm_640x480x8bus.sel;
+	    outbus_dat_o = gm_640x480x8bus.dat_m;
 	  end
 	default: // 720x400 text mode
 	  begin
@@ -146,7 +169,7 @@ module vga_master
     begin
       if (rst_i)
 	begin
-	  setupreg <= 32'h00;
+	  setupreg <= 32'h01;
 	  inbus_dat_o <= 32'h0;
 	  cursorpos <= 32'h0;
 	  cursorcolor <= 24'ha0a0a0;
@@ -169,6 +192,7 @@ module vga_master
       cursorpos_next = cursorpos;
       cursorcolor_next = cursorcolor;
       inbus_dat_o_next = inbus_dat_o;
+      palettebus.stb = 1'h0;
       
       case (sstate)
 	SS_IDLE:
@@ -237,15 +261,26 @@ module vga_master
               default:
 		sstate_next = SS_DONE;
             endcase
-	SS_PALETTE: sstate_next = SS_DONE;
-	SS_PALETTE2: sstate_next = SS_DONE;
+	SS_PALETTE: 
+	  begin
+	    palettebus.stb = 1'h1;
+	    if (~inbus.we)
+	      inbus_dat_o_next = 32'hbeefdead;
+	    sstate_next = SS_DONE;
+	  end
+	SS_PALETTE2:
+	  begin
+	    if (~inbus.we)
+	      inbus_dat_o_next = 32'hdeadbeef;
+	    sstate_next = SS_DONE;
+	  end
 	SS_FONT: sstate_next = SS_DONE;
 	SS_DONE: sstate_next = SS_IDLE;
       endcase
     end
-
+  
   textdrv #(.BPP(BPP)) textdriver0(.clk_i(clk_i),
-				   .rst_i(rst_i | (gm_active != 2'h2)),
+				   .rst_i(rst_i | (gm_active != 3'h5)),
 				   .blank_n(blank28_n),
 				   .red(td_r),
 				   .green(td_g),
@@ -258,29 +293,31 @@ module vga_master
 				   .cursorcolor(cursorcolor),
 				   .bus(textbus.master));
   
-  gm_mono graphicsdriver0(.clk_i(clk_i),
-			  .rst_i(rst_i | (gm_active != 2'h0)),
+  gm_640x480x1 graphicsdriver0(.clk_i(clk_i),
+			  .rst_i(rst_i | (gm_active != 3'h0)),
 			  .vs(vs25),
 			  .hs(hs25),
 			  .blank_n(blank25_n),
 			  .video_clk_i(vga_clock25),
 			  .video_rst_i(vga_rst_i),
-			  .red(gm_mono_r),
-			  .green(gm_mono_g),
-			  .blue(gm_mono_b),
-			  .bus(gm_monobus.master));
+			  .red(gm_640x480x1_r),
+			  .green(gm_640x480x1_g),
+			  .blue(gm_640x480x1_b),
+			  .bus(gm_640x480x1bus.master));
 
-  gm_new graphicsdriver1(.clk_i(clk_i),
-			 .rst_i(rst_i | (gm_active != 2'h1)),
-			 .vs(vs_13h),
-			 .hs(hs_13h),
-			 .blank_n(blank_13h_n),
-			 .video_clk_i(vga_clock25),
-			 .video_rst_i(vga_rst_i),
-			 .red(gm_13h_r),
-			 .green(gm_13h_g),
-			 .blue(gm_13h_b),
-			 .bus(gm_13hbus.master));
+  gm_640x480x8 graphicsdriver1(.clk_i(clk_i),
+			       .rst_i(rst_i | (gm_active != 3'h1 &&
+					       gm_active != 3'h2)),
+			       .color(gm_active == 3'h1),
+			       .vs(vs_13h),
+			       .hs(hs_13h),
+			       .blank_n(blank_13h_n),
+			       .video_clk_i(vga_clock25),
+			       .video_rst_i(vga_rst_i),
+			       .red(gm_640x480x8_r),
+			       .green(gm_640x480x8_g),
+			       .blue(gm_640x480x8_b),
+			       .bus(gm_640x480x8bus.master),
+			       .palette_bus(palettebus.slave));
 
-  
 endmodule
